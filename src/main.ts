28bfe7e93de03a314d5a77a27cfc563b2da16207
OrnellaTech <ornellakoffi05@gmail.com>
2026-03-18 01:44:22 +0000

    Ajout de log dans le main.ts et des chemins de prod dans angular.json

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { environment } from './environments/environment';
+
+console.log('ENV in main.ts:', environment);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
 
 
 
